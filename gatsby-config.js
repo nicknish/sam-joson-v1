9d7e7ddb17e55ef515b9bb629a07ab9437dcda0e
nicknish <nickjnish@gmail.com>
2018-07-01 12:23:30 -0700

    Remove unnecessary prismJS

--- gatsby-config.js
+++ gatsby-config.js
@@ -46,9 +46,6 @@ module.exports = {
       options: {
         plugins: [
           {
-            resolve: `gatsby-remark-prismjs`
-          },
-          {
             resolve: `gatsby-remark-images-contentful`,
             options: {
               maxWidth: 650,
