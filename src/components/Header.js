38e3083ca7e6c82c3e13c82e6d7010036d565307
nicknish <nickjnish@gmail.com>
2018-07-03 12:40:22 -0700

    Update Header with new logo, global fonts, and variables

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,14 +1,13 @@
 import React from 'react';
 import Link from 'gatsby-link';
+import Logo from '../images/logo_v1.png';
 import FaBars from 'react-icons/lib/fa/bars';
 
 export default ({ handleToggleMenu }) => (
   <header className="siteHeader">
-    <span className="siteHeader-title">
-      <Link to="/" className="siteHeader-titleCopy">
-        Sam Joson
-      </Link>
-    </span>
+    <Link to="/" className="siteHeader-logoLink">
+      <img src={Logo} alt="Sam Scratch logo" className="siteHeader-logo" />
+    </Link>
     <button className="siteHeader-menuBtn" onClick={handleToggleMenu}>
       <FaBars />
     </button>
