1999e97ca9b715318126fa181c3b9c7b8bd0750a
nicknish <nickjnish@gmail.com>
2018-07-03 14:58:04 -0700

    init commit

--- src/utils/typography.js
+++ src/utils/typography.js
@@ -2,6 +2,16 @@ import Typography from 'typography';
 import moragaTheme from 'typography-theme-moraga';
 
 moragaTheme.overrideThemeStyles = ({ rhythm }, options) => ({
+  // googleFonts: [
+  //   {
+  //     name: 'Amatic SC',
+  //     styles: ['400, 700']
+  //   },
+  //   {
+  //     name: 'Crimson Text',
+  //     styles: ['400', '400i', '700', '700i']
+  //   }
+  // ],
   a: {
     color: 'rgba(0,0,0,.8)'
   },
