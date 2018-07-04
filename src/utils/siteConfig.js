1e28735973711965c3a4c486a9342d461f3880b7
nicknish <nickjnish@gmail.com>
2018-07-03 21:49:43 -0700

    Update site config paths

--- src/utils/siteConfig.js
+++ src/utils/siteConfig.js
@@ -8,10 +8,10 @@ module.exports = {
   authorUrl: 'https://samjoson.com/about/', // URL used for author and publisher schema, can be a social profile or other personal site
   userTwitter: null, // Change for Twitter Cards
   shortTitle: null, // Used for App manifest e.g. Mobile Home Screen
-  shareImage: 'logos/share.jpg', // Open Graph Default Share Image. 1200x1200 is recommended
+  shareImage: '/logos/share.jpg', // Open Graph Default Share Image. 1200x1200 is recommended
   shareImageWidth: 900, // Change to the width of your default share image
   shareImageHeight: 600, // Change to the height of your default share image
-  siteLogo: 'logos/logo-512.png', // Logo used for SEO, RSS, and App manifest
+  siteLogo: '/logos/logo-512.png', // Logo used for SEO, RSS, and App manifest
   backgroundColor: '#e9e9e9', // Used for Offline Manifest
   themeColor: '#121212', // Used for Offline Manifest
   copyright: 'Copyright © 2018 Samantha Joson' // Copyright string for the RSS feed
