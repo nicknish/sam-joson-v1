f15e63ed2d57eaeea38dd0fbcb7e44fd553f0df3
nicknish <nickjnish@gmail.com>
2018-07-05 22:57:18 -0700

    Add LinkedIn to sidebar

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,38 +1,47 @@
 import React from 'react';
 import Link from 'gatsby-link';
+import IoSocialLinkedin from 'react-icons/lib/io/social-linkedin';
+
+import config from '../utils/siteConfig';
 import Paw from './svg/Paw';
 
-const SidebarLink = props => (
-  <Link className="sidebar-link" activeClassName="is-active" {...props}>
-    <Paw className="sidebar-linkPaw" />
-    {props.children}
-  </Link>
+const SidebarItem = props => (
+  <li className="sidebar-navItem">
+    <Link className="sidebar-link" activeClassName="is-active" {...props}>
+      <Paw className="sidebar-linkPaw" />
+      {props.children}
+    </Link>
+  </li>
+);
+
+const SidebarSocialItem = props => (
+  <li className="social-item">
+    <a href={props.to}>{props.children}</a>
+  </li>
 );
 
 export default ({ menuOpen, handleCloseMenu }) => (
   <aside className={`sidebar ${menuOpen ? 'is-active' : ''}`}>
     <nav>
-      <ul>
-        <li>
-          <SidebarLink to="/" onClick={handleCloseMenu} exact={true}>
-            Home
-          </SidebarLink>
-        </li>
-        <li>
-          <SidebarLink to="/about" onClick={handleCloseMenu}>
-            About
-          </SidebarLink>
-        </li>
-        <li>
-          <SidebarLink to="/portfolio" onClick={handleCloseMenu}>
-            Portfolio
-          </SidebarLink>
-        </li>
-        <li>
-          <SidebarLink to="/contact" onClick={handleCloseMenu}>
-            Contact
-          </SidebarLink>
-        </li>
+      <ul className="sidebar-nav">
+        <SidebarItem to="/" onClick={handleCloseMenu} exact={true}>
+          Home
+        </SidebarItem>
+        <SidebarItem to="/about" onClick={handleCloseMenu}>
+          About
+        </SidebarItem>
+        <SidebarItem to="/portfolio" onClick={handleCloseMenu}>
+          Portfolio
+        </SidebarItem>
+        <SidebarItem to="/contact" onClick={handleCloseMenu}>
+          Contact
+        </SidebarItem>
+      </ul>
+
+      <ul className="sidebar-social social">
+        <SidebarSocialItem to={config.socialLinkedIn}>
+          <IoSocialLinkedin className="social-icon social-icon--linkedin" />
+        </SidebarSocialItem>
       </ul>
     </nav>
   </aside>
--- src/utils/siteConfig.js
+++ src/utils/siteConfig.js
@@ -3,9 +3,9 @@ module.exports = {
   siteTitleAlt: null, // This allows an alternative site title for SEO schema.
   publisher: null, // Organization name used for SEO schema
   siteDescription: 'The blog of Sam Joson.',
-  siteUrl: 'https://samjoson.com', // Site domain. Do not include a trailing slash! If you wish to use a path prefix you can read more about that here: https://www.gatsbyjs.org/docs/path-prefix/
+  siteUrl: 'https://samjoson.com', // Site domain. Do not include a trailing slash!
   author: 'Sam Joson', // Author for RSS author segment and SEO schema
-  authorUrl: 'https://samjoson.com/about/', // URL used for author and publisher schema, can be a social profile or other personal site
+  authorUrl: 'https://samjoson.com/about', // URL used for author and publisher schema, can be a social profile or other personal site
   userTwitter: null, // Change for Twitter Cards
   shortTitle: null, // Used for App manifest e.g. Mobile Home Screen
   shareImage: '/logos/share.jpg', // Open Graph Default Share Image. 1200x1200 is recommended
@@ -14,5 +14,7 @@ module.exports = {
   siteLogo: '/logos/logo-512.png', // Logo used for SEO, RSS, and App manifest
   backgroundColor: '#e9e9e9', // Used for Offline Manifest
   themeColor: '#121212', // Used for Offline Manifest
-  copyright: 'Copyright © 2018 Samantha Joson' // Copyright string for the RSS feed
+  copyright: 'Copyright © 2018 Samantha Joson', // Copyright string for the RSS feed
+  socialEmail: 'samanthajoson856@gmail.com',
+  socialLinkedIn: 'https://linkedin.com/in/samjoson'
 };
