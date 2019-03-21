7ecb570c0ef7048d4ad29391c77854fc5cffd7ef
nicknish <nickjnish@gmail.com>
2019-03-20 20:33:43 -0700

    Add Embedly script to load YouTube videos

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -48,6 +48,20 @@ export class Layout extends React.Component {
           <meta property="og:locale" content="en_US" />
           <meta property="og:type" content="website" />
           <meta property="og:site_name" content={config.siteTitle} />
+
+          {/* Embedly for YouTube videos */}
+          <script>{`
+            (function(w, d){
+            var id='embedly-platform', n = 'script';
+            if (!d.getElementById(id)){
+              w.embedly = w.embedly || function() {(w.embedly.q = w.embedly.q || []).push(arguments);};
+              var e = d.createElement(n); e.id = id; e.async=1;
+              e.src = ('https:' === document.location.protocol ? 'https' : 'http') + '://cdn.embedly.com/widgets/platform.js';
+              var s = d.getElementsByTagName(n)[0];
+              s.parentNode.insertBefore(e, s);
+            }
+            })(window, document);
+          `}</script>
         </Helmet>
 
         <Header handleToggleMenu={this.handleToggleMenu} />
